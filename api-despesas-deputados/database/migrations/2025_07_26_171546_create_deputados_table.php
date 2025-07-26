9a27deeddd61594c117efbd7d733a8927f11c68d
carloshipol <carlaogaruda30@gmail.com>
2025-07-26 14:46:04 -0300

    fix: correct database connection and table configuration

---
+++ api-despesas-deputados/database/migrations/2025_07_26_171546_create_deputados_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deputados', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_camara')->unique();
+            $table->string('nome');
+            $table->string('sigla_partido')->nullable();
+            $table->string('sigla_uf')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deputados');
+    }
+};
